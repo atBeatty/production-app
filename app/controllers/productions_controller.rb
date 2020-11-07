af37c61b0d8ae751f04f577b5d9e7d2108c145ed
atBeatty <adam.t.beatt@gmail.com>
2020-11-06 21:01:14 -0800

    edit and update productions_controller

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -11,10 +11,34 @@ class ProductionsController < ApplicationController
         set_production
     end
 
+    def create
+        binding.pry
+    end
+
+    def edit
+        @production = Production.find_by_id(params[:id])
+    end
+
+    def update
+        p = Production.find(params[:id])
+
+        binding.pry
+        p.update(title: params[:production][:title])
+        binding.pry
+        redirect_to productions_path
+
+    end
+
+
     private
     def set_production
         @production = Production.find_by_id(params[:id])
     end
+
+    def production_params
+        params.require(:production).permit(:title, :contract)
+    end
+
 
 
 
