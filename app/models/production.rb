36e98b6a387c2a6c2dd186d2d4b8d860631fd79e
atBeatty <adam.t.beatt@gmail.com>
2020-11-12 11:59:03 -0800

    fixed create new production client_name field

--- app/models/production.rb
+++ app/models/production.rb
@@ -11,28 +11,21 @@ class Production < ApplicationRecord
   end
 
   def client_name=(name)
-    #client exist?
-      #client.productions << production
-    #if not
-      #create client
-      #production.client = created client
     client = Client.find_by_id(self.client.id)
-
     if client
       client.update(name: name)
     else
       client = Client.create(name: name)
       self.client = client
-      
     end
-    # client = Client.find_by(name: name)
-    # self.client_id = client.id
-
   end
 
   def client_name
-  #   self.client.name ||= ""
-    self.client.name
+    if self.client
+      self.client.name
+    else
+      ""
+    end
   end
 
 
